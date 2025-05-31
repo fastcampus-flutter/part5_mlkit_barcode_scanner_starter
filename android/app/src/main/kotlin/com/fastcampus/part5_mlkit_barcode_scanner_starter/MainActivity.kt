3ac50ef92e791180986c8a1e726f39bb6c267c1d
Dreamwalker <aristojeff@gmail.com>
2025-05-31 21:36:49 +0900

    patch: 2025-05-31

--- android/app/src/main/kotlin/com/fastcampus/part5_mlkit_barcode_scanner_starter/MainActivity.kt
+++ android/app/src/main/kotlin/com/fastcampus/part5_mlkit_barcode_scanner_starter/MainActivity.kt
@@ -2,5 +2,4 @@ package com.fastcampus.part5_mlkit_barcode_scanner_starter
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
